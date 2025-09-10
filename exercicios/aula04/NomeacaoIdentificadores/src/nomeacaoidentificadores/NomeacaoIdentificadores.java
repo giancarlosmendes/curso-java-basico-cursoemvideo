56ef2a8da584bf52660c791f08719392636b488e
Giancarlos Mendes <gmendescamara@gmail.com>
2025-09-10 05:33:39 -0300

    Atualiza aula04

---
+++ exercicios/aula04/NomeacaoIdentificadores/src/nomeacaoidentificadores/NomeacaoIdentificadores.java
@@ -0,0 +1,42 @@
+//package = pacote
+package nomeacaoidentificadores;
+
+       //class = classe
+public class NomeacaoIdentificadores {
+
+                       //main = método principal
+    public static void main(String[] args) {
+        
+        /* HIERARQUIA:
+        Pacotes
+        └── Classes
+            └── Métodos
+        */
+        
+        /* NOMENCLATURAS
+        -> Pacotes: 
+                - São compostos de uma ou mais classes;
+                - Devem ser escritos todo em letras minúsculas;
+                - Se tiverem mais de uma palavra, podem ser separadas por ponto;
+                    - Ex: primeiroprograma, alunoscursoemvideo, com.cursoemvideo.alunos
+        
+        -> Classes/Interfaces: 
+                - São compostas de um ou mais métodos;
+                - Devem ter a primeira letra de cada palavra em maíuscula;
+                    - Ex: AlunosCursoEmVideo
+        
+        -> Métodos/Atributos/Variáveis:
+                - Devem ter a primeira letra em minúscula. Caso tenha mais de uma
+                  palavra, o começo de cada uma deve ser em maiúscula;
+                    - Ex: main, nomeAluno, mediaPrimeiroBimestre, lancarNotaAluno
+        
+        -> Constantes:
+                - Devem ter todas as letras em maiúsculas;
+                - Se tiverem mais de uma palavra, devem ser separadas por sublinhado _;
+                    - Ex: VALOR_DE_PI
+        */
+        
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
